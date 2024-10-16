c7222a82e92827cf98c658be875ecad19922bfee
semihkeskindev <semihkeskindev@gmail.com>
2024-10-16 15:45:36 +0300

    analytics tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,6 +42,15 @@
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-94CFZ6NES6"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-94CFZ6NES6');
+</script>
 <body>
 @inertia
 </body>
